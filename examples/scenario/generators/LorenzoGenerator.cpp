699df68422a268a540c49bb0660cfba9e07ca727
Zimmerer <lorenzo.zimmerer@mymail.champlain.edu>
2022-12-05 14:12:45 -0500

    adding simple biome colors to genererator

--- examples/scenario/generators/LorenzoGenerator.cpp
+++ examples/scenario/generators/LorenzoGenerator.cpp
@@ -1,12 +1,52 @@
 #include "LorenzoGenerator.h"
+#include "../FastNoiseLite.h"
+#include "../PerlinNoise.hpp"
+#include <iostream>
 
 std::vector<Color32> LorenzoGenerator::Generate(int sideSize, float displacement) {
 
   std::vector<Color32> cols;
+  FastNoiseLite base;
+  base.SetFractalOctaves(3);
+  base.SetNoiseType(FastNoiseLite::NoiseType_OpenSimplex2S);
+  base.SetFractalType(FastNoiseLite::FractalType_FBm);
+  
+  FastNoiseLite cellular;
+  cellular.SetNoiseType(FastNoiseLite::NoiseType_Cellular);
+  cellular.SetFractalOctaves(3);
 
+  for (int i = 0; i < sideSize; i++) {
+    for (int j = 0; j < sideSize; j++) {
+        
+        float height = ((base.GetNoise((float)i, (float)j, displacement * 50) + 1) / 2);
+        float humidity = ((cellular.GetNoise((float)j, (float)i, displacement * 50) + 1) / 2);
 
+        auto col = getColor(height, humidity);
+
+        cols.emplace_back(col);
+    }
+  }
 
   return cols;
 }
 
 std::string LorenzoGenerator::GetName() { return "LORENZOGEN"; }
+
+Color32 LorenzoGenerator::getColor(float height, float humidity) {
+
+    if (height < seaLevel) {
+      return Color::DarkTurquoise;
+    } else if (height < beachLevel) {
+      return Color::Beige;
+    } else if (height < grassLevel) {
+      return Color::LightGreen;
+    } else if (height < forestLevel) {
+      return Color::ForestGreen;
+    } else if (height < rockLevel) {
+      return Color::LightSlateGray;
+    } else if (height < snowLevel) {
+      return Color::Snow;
+    } 
+
+    return Color::HotPink;    
+}
--- examples/scenario/generators/LorenzoGenerator.h
+++ examples/scenario/generators/LorenzoGenerator.h
@@ -7,6 +7,16 @@ class LorenzoGenerator : public ScenarioGeneratorBase {
  public:
   std::vector<Color32> Generate(int sideSize, float displacement = 0) override;
   std::string GetName() override;
+  Color32 getColor(float height, float humidity);
+
+ private:
+  float seaLevel = 0.4;
+  float beachLevel = 0.5;
+  float grassLevel = 0.55;
+  float forestLevel = 0.65;
+  float rockLevel = 0.8;
+  float snowLevel = 0.9;
+
 };
 
 #endif  // LORENZOGEN_H
